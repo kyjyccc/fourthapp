0023690956bb728f072a4bdf3a7fd8cf1349d856
ï¼âkyjyccc <993978731@qq.com>
2020-10-27 21:35:38 +0800

    实现了篮球计分

--- app/src/main/java/com/swufe/fourthapp/TemperatureActivity.java
+++ app/src/main/java/com/swufe/fourthapp/TemperatureActivity.java
@@ -14,7 +14,7 @@ public class TemperatureActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_temperature);
         out = findViewById(R.id.out);
         inp = findViewById(R.id.inp);
     }
