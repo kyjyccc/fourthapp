8d820782e1f260781bcd9504be8428f48d6f8a02
ï¼âkyjyccc <993978731@qq.com>
2020-10-27 21:13:59 +0800

    实现了温度转换

---
+++ app/src/main/java/com/swufe/fourthapp/TemperatureActivity.java
@@ -0,0 +1,30 @@
+package com.swufe.fourthapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class TemperatureActivity extends AppCompatActivity {
+
+    TextView out;
+    EditText inp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        out = findViewById(R.id.out);
+        inp = findViewById(R.id.inp);
+    }
+
+    public void btn(View v){
+        String str1 = inp.getText().toString();
+        Double a = Double.parseDouble(str1);
+        Double b = a*1.8+32.0;
+        String str2 = String.valueOf(b);
+        out.setText("华氏温度为：" + str2);
+
+    }
+}
